f4c6775523c61cc8da4864be938c908e925a76e3
Nelson <74392133+UncleBruh@users.noreply.github.com>
2026-04-09 17:49:52 +0700

    4

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -1,41 +1,39 @@
 <nav x-data="{ open: false }" class="bg-white border-b border-gray-100">
-    <!-- Primary Navigation Menu -->
     <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div class="flex justify-between h-16">
             <div class="flex">
-                <!-- Logo -->
                 <div class="shrink-0 flex items-center">
                     <a href="{{ route('dashboard') }}">
-                        <x-application-logo class="block h-9 w-auto fill-current text-gray-800" />
+                        <span class="font-bold text-xl tracking-wider text-indigo-600">INVENTORI</span>
                     </a>
                 </div>
 
-                <!-- Navigation Links -->
-                <<div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
-                        <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
-                            {{ __('Dashboard') }}
-                        </x-nav-link>
+                <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
+                    <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
+                        {{ __('Dashboard') }}
+                    </x-nav-link>
 
-                        <x-nav-link :href="route('makanan.index')" :active="request()->routeIs('makanan.*')">
-                            {{ __('Daftar Jajanan') }}
-                        </x-nav-link>
-                        <x-nav-link :href="route('log.create')" :active="request()->routeIs('log.create')">
-                            {{ __('Mutasi Stok') }}
-                        </x-nav-link>
+                    <x-nav-link :href="route('makanan.index')" :active="request()->routeIs('makanan.*')">
+                        {{ __('Daftar Jajanan') }}
+                    </x-nav-link>
+                    
+                    <x-nav-link :href="route('log.create')" :active="request()->routeIs('log.create')">
+                        {{ __('Mutasi Stok') }}
+                    </x-nav-link>
 
-                        @if(Auth::user()->role === 'Pemilik')
-                        <x-nav-link :href="route('log.aktivitas')" :active="request()->routeIs('log.aktivitas')">
-                            {{ __('Laporan Log') }}
-                        </x-nav-link>
-                        @endif
-                    </div>
+                    @if(Auth::user()->role === 'Pemilik')
+                    <x-nav-link :href="route('log.aktivitas')" :active="request()->routeIs('log.aktivitas')">
+                        {{ __('Laporan Log') }}
+                    </x-nav-link>
+                    @endif
+                </div>
+            </div>
 
-            <!-- Settings Dropdown -->
             <div class="hidden sm:flex sm:items-center sm:ms-6">
                 <x-dropdown align="right" width="48">
                     <x-slot name="trigger">
                         <button class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 bg-white hover:text-gray-700 focus:outline-none transition ease-in-out duration-150">
-                            <div>{{ Auth::user()->name }}</div>
+                            <div>{{ Auth::user()->username }}</div>
 
                             <div class="ms-1">
                                 <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
@@ -46,11 +44,6 @@
                     </x-slot>
 
                     <x-slot name="content">
-                        <x-dropdown-link :href="route('profile.edit')">
-                            {{ __('Profile') }}
-                        </x-dropdown-link>
-
-                        <!-- Authentication -->
                         <form method="POST" action="{{ route('logout') }}">
                             @csrf
 
@@ -64,7 +57,6 @@
                 </x-dropdown>
             </div>
 
-            <!-- Hamburger -->
             <div class="-me-2 flex items-center sm:hidden">
                 <button @click="open = ! open" class="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 focus:text-gray-500 transition duration-150 ease-in-out">
                     <svg class="h-6 w-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
@@ -76,27 +68,34 @@
         </div>
     </div>
 
-    <!-- Responsive Navigation Menu -->
     <div :class="{'block': open, 'hidden': ! open}" class="hidden sm:hidden">
         <div class="pt-2 pb-3 space-y-1">
             <x-responsive-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                 {{ __('Dashboard') }}
             </x-responsive-nav-link>
+
+            <x-responsive-nav-link :href="route('makanan.index')" :active="request()->routeIs('makanan.*')">
+                {{ __('Daftar Jajanan') }}
+            </x-responsive-nav-link>
+            
+            <x-responsive-nav-link :href="route('log.create')" :active="request()->routeIs('log.create')">
+                {{ __('Mutasi Stok') }}
+            </x-responsive-nav-link>
+            
+            @if(Auth::user()->role === 'Pemilik')
+            <x-responsive-nav-link :href="route('log.aktivitas')" :active="request()->routeIs('log.aktivitas')">
+                {{ __('Laporan Log') }}
+            </x-responsive-nav-link>
+            @endif
         </div>
 
-        <!-- Responsive Settings Options -->
         <div class="pt-4 pb-1 border-t border-gray-200">
             <div class="px-4">
-                <div class="font-medium text-base text-gray-800">{{ Auth::user()->name }}</div>
-                <div class="font-medium text-sm text-gray-500">{{ Auth::user()->email }}</div>
+                <div class="font-medium text-base text-gray-800">{{ Auth::user()->username }}</div>
+                <div class="font-medium text-sm text-gray-500">{{ Auth::user()->role }}</div>
             </div>
 
             <div class="mt-3 space-y-1">
-                <x-responsive-nav-link :href="route('profile.edit')">
-                    {{ __('Profile') }}
-                </x-responsive-nav-link>
-
-                <!-- Authentication -->
                 <form method="POST" action="{{ route('logout') }}">
                     @csrf
 
